cd96384059cc3c221f34d1ca590e50a03975a93c
MORE Robotics <robotics1714@gmail.com>
2023-03-31 18:49:02 -0500

    Added Recommended code from Pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -355,7 +358,13 @@ public class RobotContainer {
         //followTrajectoryCommand(examplePath, true)
     );   
     }
+//want to test
 
+PathPlannerTrajectory traj1 = PathPlanner.generatePath(
+    new PathConstraints(4, 3), 
+    new PathPoint(new Translation2d(1.0, 1.0), Rotation2d.fromDegrees(0)), // position, heading
+    new PathPoint(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(45)) // position, heading
+);
 
     ////////////////////////////////////
     
